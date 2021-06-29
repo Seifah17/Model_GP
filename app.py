ea7727370752ad41cbfc59722a0bc47b3f66848d
Seif alden hany hamdy <52981447+Seifah17@users.noreply.github.com>
2021-06-29 16:28:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 def index():
     return "Flask server"
 
-@app.route('/', methods=['POST'])
+@app.route('/postdata', methods=['POST'])
 def postdata():
     user = request.get_json()
     print(user)
